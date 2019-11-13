9f400223e5edb98ebac62562f6d273678c5032ac
yanakence <yanakence@yanah.jp>
2019-11-13 12:24:15 +0900

    feat: ログアウトを実装した

---
+++ src/diary/controller/LogoutServlet.java
@@ -0,0 +1,27 @@
+package diary.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Get Session
+        HttpSession session = request.getSession(false);
+
+        // If a session exists
+        if(session != null) {
+            session.invalidate();
+        }
+
+        response.sendRedirect("login?code=2");
+        return;
+    }
+}
