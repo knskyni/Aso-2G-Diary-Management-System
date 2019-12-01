27fd95270a921a0da4e566bd14ff550426808e3e
yanakence <yanakence@yanah.jp>
2019-12-01 23:01:37 +0900

    fix: クラス名をちゃんと取得できていなかったのを修正

--- src/diary/dao/StudentDao.java
+++ src/diary/dao/StudentDao.java
@@ -8,7 +8,7 @@ import diary.beans.UserInfoBeans;
 import diary.util.Password;
 
 public class StudentDao extends Dao {
-    private final String loginSQL = "SELECT `students`.`student_id`, `students`.`first_name`, `students`.`last_name`, `students`.`password`, `students`.`class_code`, `classes`.`class_name` FROM `students` INNER JOIN `classes` ON `students`.`class_code` = `classes`.`class_code` WHERE `students`.`student_id` = ?;";
+    private final String loginSQL = "SELECT `students`.`student_id`, `students`.`first_name`, `students`.`last_name`, `students`.`password`, `students`.`class_code`, `courses`.`course_name`, `classes`.`grade`, `classes`.`class_name` FROM `students` INNER JOIN `classes` ON `students`.`class_code` = `classes`.`class_code` INNER JOIN `courses` ON `classes`.`course_code` = `courses`.`course_code` WHERE `students`.`student_id` = ?;";
 
     public UserInfoBeans login(String userId, String password) {
         // If not connected to DB, return null
@@ -33,7 +33,7 @@ public class StudentDao extends Dao {
                     userInfo.setFirstName(rs.getString("first_name"));
                     userInfo.setLastName(rs.getString("last_name"));
                     userInfo.setClassId(rs.getInt("class_code"));
-                    userInfo.setClassName(rs.getString("class_name"));
+                    userInfo.setClassName(rs.getString("course_name") + rs.getString("grade") + rs.getString("class_name"));
                     userInfo.setType("student");
                 }
             }
