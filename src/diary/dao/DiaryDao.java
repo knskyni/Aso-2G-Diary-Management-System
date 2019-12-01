3238ca14549281c9b37e75ac34914492637e2065
yanakence <yanakence@yanah.jp>
2019-12-01 23:13:38 +0900

    fix: 日誌一覧にて学生のユーザーIDを取得できていなかったのを修正

--- src/diary/dao/DiaryDao.java
+++ src/diary/dao/DiaryDao.java
@@ -9,12 +9,62 @@ import java.util.List;
 import diary.beans.DiaryInfoBeans;
 
 public class DiaryDao extends Dao {
-    public final String getListSQL = "SELECT `diaries`.`id`, `students`.`last_name` AS `student_last_name`, `students`.`first_name` AS `student_first_name`, `courses`.`course_name`, `classes`.`grade`, `classes`.`class_name`, `diaries`.`date`, `diaries`.`regist_time`, `diaries`.`update_time`, `diaries`.`good_comment`, `diaries`.`bad_comment`, `diaries`.`about_comment`, `teachers`.`last_name` AS `teacher_last_name`, `teachers`.`first_name` AS `teacher_first_name`, `diaries`.`teacher_comment` FROM `diaries` INNER JOIN `students` ON `diaries`.`student_id` = `students`.`student_id` INNER JOIN `classes` ON `diaries`.`class_code` = `classes`.`class_code` INNER JOIN `courses` ON `classes`.`course_code` = `courses`.`course_code` LEFT OUTER JOIN `teachers` ON `diaries`.`teacher_id` = `teachers`.`teacher_id` WHERE `diaries`.`class_code` = ? AND `status` = 'public';";
+    public final String getAllListSQL = "SELECT `diaries`.`id`, `students`.`last_name` AS `student_last_name`, `students`.`first_name` AS `student_first_name`, `courses`.`course_name`, `classes`.`grade`, `classes`.`class_name`, `diaries`.`date`, `diaries`.`regist_time`, `diaries`.`update_time`, `diaries`.`good_comment`, `diaries`.`bad_comment`, `diaries`.`about_comment`, `teachers`.`last_name` AS `teacher_last_name`, `teachers`.`first_name` AS `teacher_first_name`, `diaries`.`teacher_comment` FROM `diaries` INNER JOIN `students` ON `diaries`.`student_id` = `students`.`student_id` INNER JOIN `classes` ON `diaries`.`class_code` = `classes`.`class_code` INNER JOIN `courses` ON `classes`.`course_code` = `courses`.`course_code` LEFT OUTER JOIN `teachers` ON `diaries`.`teacher_id` = `teachers`.`teacher_id` WHERE `status` = 'public';";
+    public final String getListSQL = "SELECT `diaries`.`id`, `diaries`.`student_id`, `students`.`last_name` AS `student_last_name`, `students`.`first_name` AS `student_first_name`, `courses`.`course_name`, `classes`.`grade`, `classes`.`class_name`, `diaries`.`date`, `diaries`.`regist_time`, `diaries`.`update_time`, `diaries`.`good_comment`, `diaries`.`bad_comment`, `diaries`.`about_comment`, `teachers`.`last_name` AS `teacher_last_name`, `teachers`.`first_name` AS `teacher_first_name`, `diaries`.`teacher_comment` FROM `diaries` INNER JOIN `students` ON `diaries`.`student_id` = `students`.`student_id` INNER JOIN `classes` ON `diaries`.`class_code` = `classes`.`class_code` INNER JOIN `courses` ON `classes`.`course_code` = `courses`.`course_code` LEFT OUTER JOIN `teachers` ON `diaries`.`teacher_id` = `teachers`.`teacher_id` WHERE `diaries`.`class_code` = ? AND `status` = 'public';";
     public final String insertCheckSQL = "SELECT COUNT(*) AS `count` FROM `diaries` WHERE `class_code` = ? AND `date` = ? AND `status` = 'public';";
     public final String insertSQL = "INSERT INTO `diaries`(`class_code`, `date`, `student_id`, `regist_time`, `update_time`, `good_comment`, `bad_comment`, `about_comment`) VALUES (?, ?, ?, ?, NULL, ?, ?, ?);";
     public final String getDiarySQL = "SELECT `student_id`, `date`, `good_comment`, `bad_comment`, `about_comment` FROM `diaries` WHERE `id` = ? AND `status` = 'public';";
     public final String updateSQL = "UPDATE `diaries` SET `date` = ?, `update_time` = ?, `good_comment` = ?, `bad_comment` = ?, `about_comment` = ? WHERE `id` = ? AND `student_id` = ?;";
     public final String deleteSQL = "UPDATE `diaries` SET `status` = 'deleted' WHERE `id` = ? AND `student_id` = ?;";
+
+    public List<DiaryInfoBeans> getList() {
+        // If not connected to DB, return null
+        if(con == null) {
+            System.out.println("データベースに接続していません。");
+            return null;
+        }
+
+        // SQL Execute
+        PreparedStatement stmt = null;
+        DiaryInfoBeans diaryInfo = null;
+        List<DiaryInfoBeans> diaryList = new ArrayList<DiaryInfoBeans>();
+        try {
+            stmt = con.prepareStatement(getAllListSQL);
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()) {
+                diaryInfo = new DiaryInfoBeans();
+                diaryInfo.setDiaryId(rs.getInt("id"));
+                diaryInfo.setUserName(rs.getString("student_last_name") + " " + rs.getString("student_first_name"));
+                diaryInfo.setClassName(rs.getString("course_name") + rs.getString("grade") + rs.getString("class_name"));
+                diaryInfo.setDate(rs.getDate("date"));
+                diaryInfo.setRegistTime(rs.getTimestamp("regist_time"));
+                diaryInfo.setUpdateTime(rs.getTimestamp("update_time"));
+                diaryInfo.setGoodComment(rs.getString("good_comment"));
+                diaryInfo.setBadComment(rs.getString("bad_comment"));
+                diaryInfo.setAboutComment(rs.getString("about_comment"));
+
+                if(rs.getString("teacher_last_name") == null || rs.getString("teacher_first_name") == null) {
+                    diaryInfo.setTeacherName(null);
+                } else {
+                    diaryInfo.setTeacherName(rs.getString("teacher_last_name") + " " + rs.getString("teacher_first_name"));
+                }
+
+                diaryInfo.setTeacherComment(rs.getString("teacher_comment"));
+
+                diaryList.add(diaryInfo);
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        // If there is no result, return null
+        if(diaryList.size() <= 0) {
+            return null;
+        }
+
+        return diaryList;
+    }
 
     public List<DiaryInfoBeans> getList(int classId) {
         // If not connected to DB, return null
@@ -35,6 +85,7 @@ public class DiaryDao extends Dao {
             while(rs.next()) {
                 diaryInfo = new DiaryInfoBeans();
                 diaryInfo.setDiaryId(rs.getInt("id"));
+                diaryInfo.setUserId(rs.getString("student_id"));
                 diaryInfo.setUserName(rs.getString("student_last_name") + " " + rs.getString("student_first_name"));
                 diaryInfo.setClassName(rs.getString("course_name") + rs.getString("grade") + rs.getString("class_name"));
                 diaryInfo.setDate(rs.getDate("date"));
